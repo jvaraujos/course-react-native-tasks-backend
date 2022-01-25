8eb8b585e6a07a716ff328a0e34b9b4d3d2101d0
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-01-24 22:33:49 -0300

    147-Resumo sobre o Express

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express= require('express')
 const app= express()
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res)=>{
-    res.status(200).send('Meu Backend')
+app.use(meujson())
+app.use(bodyParser.json())
+
+
+function meujson(){
+    return (req,res,next)=>{
+        console.log('Antes de tudo:Meu middleware...')
+        next()
+    }
+}
+
+app.get('/blabla/:valor', (req, res,next)=>{
+    console.log('Func 0')
+    res.status(200)
+    next()
 })
-
+app.post('/blabla/:valor', (req,res,next)=>{
+    console.log('Func 1')
+    res.status(200).send('<h1>Meu Backend1 - </h1>'+ req.body.nome)
+})
+app.post('/blabla/:valor', (req, res)=>{
+    console.log('Func 2')
+    res.status(200)
+})
 app.listen(3000,()=>{
     console.log('Backend executando....')
 })
