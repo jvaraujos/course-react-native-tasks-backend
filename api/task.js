6a57416db0ac61ae6eb9d90dd23c9c6e40628c06
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-01-25 14:43:34 -0300

    157-Api de tarefas tasks 02

--- api/task.js
+++ api/task.js
@@ -2,10 +2,10 @@ const moment = require('moment')
 
 module.exports = app => {
     const getTasks = (req,res) =>{
-        const date = req.query.date?req.quer.date:moment().endOf('day').toDate()
+        const date = req.query.date?req.query.date:moment().endOf('day').toDate()
 
-        app.db('users').where({
-            userId:req.query.id
+        app.db('tasks').where({
+            userId:req.user.id
         })
         .where('estimateAt','<=',date)
         .orderBy('estimateAt')
@@ -17,16 +17,20 @@ module.exports = app => {
         if(!req.body.desc.trim()){
             return res.status(400).send('Descricao e um campo obrigatorio')
         }
-        req.body.userId = req.query.id
+        req.body.userId = req.user.id
         
         app.db('tasks')
         .insert(req.body)
-        .then(_=>res.status(200).send())
+        .then(_=>res.status(204).send())
         .catch(err =>res.status(500).json(err))
     }
+    
     const remove = (req,res)=>{
         app.db('tasks')
-        .where({id:req.params.id,userId:req.user.id})
+        .where({
+            id:req.params.id,
+            userId:req.user.id
+        })
         .del()
         .then(rowsDeleted=>{
             if(rowsDeleted>0)
@@ -38,4 +42,34 @@ module.exports = app => {
             }
         }).catch(err =>res.status(500).json(err))
     }
+
+    const updateTaskDoneAt =(req,res,doneAt)=>{
+        app.db('tasks')
+        .where({
+            id:req.params.id,
+            userId:req.user.id
+        }).update({doneAt})
+        .then(_=>res.status(204).send())
+        .catch(err =>res.status(500).json(err))
+    }
+
+    const toggleTask = (req,res)=>{
+        app.db('tasks')
+        .where({
+            id:req.params.id,
+            userId:req.user.id
+        })
+        .first()
+        .then(task=>{
+            if(!task){
+                const msg =`Task com id ${req.params.id}`
+                return res.status(400).send(msg)
+            }
+            const doneAt=task.doneAt?null:new Date()
+            updateTaskDoneAt(req,res,doneAt)
+        })
+        .catch(err => res.status(500).json(err))
+    }
+
+    return{ getTasks, save, remove, toggleTask}
 }
